99a2ff4a722137b0bbf418a0f29c775baf8d83c8
Josphat Otieno <josphathumphrey5@gmail.com>
2022-05-16 09:55:17 +0300

    modified file

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const birthdayCakes = document.querySelectorAll('.birthday-cake');
+const cakes = document.querySelectorAll('.cake');
 
 // Check if in viewport
 const isInViewport = (item, offset = 200) => {
@@ -12,12 +12,11 @@ const isInViewport = (item, offset = 200) => {
 
 
 const scrollAnimation = () => {
-
-birthdayCakes.forEach((item) => {
-    if(isInViewport(item)) {
-        item.classList.add('show')
-    } 
-})
+    cakes.forEach((item) => {
+        if(isInViewport(item)) {
+            item.classList.add('show')
+        } 
+    })
 }
 
 // Events
@@ -25,4 +24,62 @@ window.addEventListener('load', scrollAnimation);
 window.addEventListener('resize', scrollAnimation);
 window.addEventListener('scroll', scrollAnimation);
 
+const arr = [[2,3,4],[4,5,6],[9,8,7]];
 
+function diagonalDifference(arr) {
+    // Write your code here
+    let leftToRight = [];
+    let rightToLeft = [];
+    
+    arr[0].forEach(num => {
+        if(num[0]) {
+            console.log(num[0])
+            leftToRight.push(num[0])
+        }
+        
+    })
+
+    console.log(leftToRight);
+    
+    arr[1].forEach(num => {
+        if(num[1]) {
+            leftToRight.push(num[1])
+        }
+        
+    })
+    
+    arr[2].forEach(num => {
+         if(num[2]) {
+            leftToRight.push(num[2])
+        }
+    })
+    
+    arr[0].forEach(num => {
+        if(num[2]) {
+            rightToLeft.push(num[2])
+        }
+        
+    })
+    
+    arr[1].forEach(num => {
+        if(num[1]) {
+            rightToLeft.push(num[1])
+        }
+        
+    })
+    
+    arr[2].forEach(num => {
+         if(num[0]) {
+            rightToLeft.push(num[0])
+        }
+    })
+    console.log(leftToRight)
+    
+    // let left = leftToRight.reduce((a,b) => a + b);
+    // let right = rightToLeft.reduce((a,b)=> a + b);
+    // let diff = left - right;
+    // return Math.abs(diff)
+}
+
+
+diagonalDifference(arr);
